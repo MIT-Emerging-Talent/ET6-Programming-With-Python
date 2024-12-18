071198604c8f2a4ab5c07b5881495c12da01f73d
Heba Shaheen <shaheenheba2000@gmail.com>
2024-12-18 22:05:01 +0300

    changecode and rename

---
+++ 3_documenting_and_testing/exercises/tests/test_sort_list.py
@@ -0,0 +1,23 @@
+import unittest
+
+from ..sort_list import sort_list
+
+
+class TestMystery5(unittest.TestCase):
+    """ """
+
+    def test_two_lists(self):
+        """it should give [1, 9, 0, 1, 4, 5] sort the first list and append it to the other"""
+        self.assertEqual(sort_list([4, 5, 1, 0], [1, 9]), [1, 9, 0, 1, 4, 5])
+
+    def test_two_empty(self):
+        """it should give []"""
+        self.assertEqual(sort_list([], []), [])
+
+    def test_list_None(self):
+        """it should give []"""
+        self.assertEqual(sort_list([], None), [])
+
+    def test_list_None_default(self):
+        """it should give []"""
+        self.assertEqual(sort_list([]), [])
