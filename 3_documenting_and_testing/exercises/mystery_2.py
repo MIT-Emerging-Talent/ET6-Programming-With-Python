fc78524039d2b6c787d559488e805f05c1bb5dea
Heba Shaheen <shaheenheba2000@gmail.com>
2024-12-18 17:17:35 +0300

    changecode

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,5 +1,52 @@
-def mystery_2(a):
-    if len(a) == 0:
-        return None
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Updated on 16 12 2024
 
-    return len(a)
+@author: Evan Cole
+@editor: Heba
+"""
+
+# --- before documenting and testing ---
+
+# def mystery_3(a, b):
+#    if a < b:
+#        return a
+#    elif a > b:
+#        return b
+#    else:
+#        return a + b
+
+# --- after documenting and testing ---
+
+
+def compare_num(number_1, number_2):
+    """Function that compers two numbers
+    then returns the lowest and if they equal it returns sum
+
+    Args:
+        number_1 : just a number
+        number_2 : just a number
+
+    Returns -> the lowest number or the sum of the twon numbers
+
+    >>> compare_num(4,5)
+    4
+
+    >>> compare_num(2,6)
+    2
+
+    >>> compare_num(2.1,2.1)
+    4.2
+
+    """
+
+    if number_1 < number_2:
+        return number_1
+    elif number_1 > number_2:
+        return number_2
+    else:
+        return number_1 + number_2
+
+
+# print(compare_num(2.1, 2.1))
