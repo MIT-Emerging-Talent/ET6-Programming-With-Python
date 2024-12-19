69d39e1a71dc12a81f5ad2c140211d3231820f24
Heba Shaheen <shaheenheba2000@gmail.com>
2024-12-19 18:12:23 +0300

    changecode and rename

---
+++ 3_documenting_and_testing/exercises/make_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 18/12/2024
+
+@author: Evan Cole
+@editor: Heba Shaheen
+"""
+# --- before documenting and testing ---
+
+
+# def mystery_6(a, b):
+#    if a == 0:
+#        return []
+#    c = []
+#    while len(c) < a:
+#        c.append(b)
+#        b = b + 1
+#    return c
+# --- after documenting and testing ---
+def mystery_6(length: int, start):
+    """Make a list with specific length and it start from a number and add +1 everytime
+
+    Args:
+        length (int): The list length that we want
+        start (_type_): a specific value to start the list with
+
+    Returns(list) -> A list with spesific length and start value
+    """
+    if length == 0:
+        return []
+
+    c = []
+    while len(c) < length:
+        c.append(start)
+        start = start + 1
+
+    return c
