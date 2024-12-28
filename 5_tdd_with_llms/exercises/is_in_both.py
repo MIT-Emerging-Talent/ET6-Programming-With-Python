9c4f084a4221923951d4bdd097c980cfb66b6e58
Heba Shaheen <shaheenheba2000@gmail.com>
2024-12-29 00:22:19 +0300

    is_in_both exercises

--- 5_tdd_with_llms/exercises/is_in_both.py
+++ 5_tdd_with_llms/exercises/is_in_both.py
@@ -1,6 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """ Is in Both
 
 Write a function that takes in a string and two lists of strings. 
 It will return true if the item is in _both_ of the lists.
 
+> I solved it on 28/ 12/ 2024
+
 """
+
+
+def is_in_both(thestring: str, list1: list, list2: list) -> bool:
+    """Fuction that return True if the string is in both list1 and list2
+
+    Args:
+        thestring (str): The string to search for
+        list1 (list): First list to search in
+        list2 (list): Second list to search in
+
+    Returns:
+        bool: return True or False
+
+    >>> is_in_both("Noor", ["Heba", "Eman", "Noor"], ["Sondos", "Noor"])
+    True
+    >>> is_in_both("333", ["123", "333", "774"], ["23", "65", "333", "532"])
+    True
+    >>> is_in_both("Mayar", ["Heba", "Eman", "Noor"], ["Sondos", "Noor"])
+    False
+    >>> is_in_both("Eman", ["Heba", "Eman", "Noor"], ["Sondos", "Noor"])
+    False
+    """
+
+    assert isinstance(thestring, str), "it should be a string"
+    assert isinstance(list1, list), "should be a list"
+    assert isinstance(list2, list), "should be a list"
+    assert len(thestring) > 0, "the string should have an item"
+    assert len(list1) > 0, "the list should have items"
+    assert len(list2) > 0, "the list should have items"
+
+    if thestring in list1 and thestring in list2:
+        return True
+    else:
+        return False
---
+++ 5_tdd_with_llms/exercises/tests/test_is_in_both.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+
+import unittest
+
+from ..is_in_both import is_in_both
+
+
+class TestIsInBoth(unittest.TestCase):
+    """Test for the is_in_both function"""
+
+    # Standard test cases
+    def test_string_in_two_list(self):
+        """It should give the string that appers in the two lists"""
+        thestring = "Heba"
+        list1 = ["Noor", "Omer", "Heba"]
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        self.assertEqual(is_in_both(thestring, list1, list2), True)
+
+    def test_string_in_one_list(self):
+        """It should give the string that appers in the two lists"""
+        thestring = "Hala"
+        list1 = ["Noor", "Hala", "Heba"]
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        self.assertEqual(is_in_both(thestring, list1, list2), False)
+
+    def test_string_not_in(self):
+        """It should give the string that appers in the two lists"""
+        thestring = "Mayar"
+        list1 = ["Noor", "Omer", "Heba"]
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        self.assertEqual(is_in_both(thestring, list1, list2), False)
+
+    def test_impty_list(self):
+        """It should raise AssertionError if a list is empty"""
+        thestring = "Heba"
+        list1 = []
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        with self.assertRaises(AssertionError):
+            is_in_both(thestring, list1, list2)
+
+    def test_impty_list_2(self):
+        """It should raise AssertionError if a list is empty"""
+        thestring = "Heba"
+        list1 = []
+        list2 = []
+        with self.assertRaises(AssertionError):
+            is_in_both(thestring, list1, list2)
+
+    def test_impty_string(self):
+        """It should raise AssertionError if the string is empty"""
+        thestring = ""
+        list1 = ["samy", "omnia"]
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        with self.assertRaises(AssertionError):
+            is_in_both(thestring, list1, list2)
+
+    def test_None_list(self):
+        """It should raise AssertionError"""
+        thestring = "Heba"
+        list1 = "Noor and Heba"
+        list2 = ["Ahmed", "Heba", "Sondos", "Haya"]
+        with self.assertRaises(AssertionError):
+            is_in_both(thestring, list1, list2)
