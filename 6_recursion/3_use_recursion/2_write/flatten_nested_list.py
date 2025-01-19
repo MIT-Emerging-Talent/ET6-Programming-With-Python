4824739a5b6e07b3fec29e4f0598a06dcfa1b1a0
Aziz <aziztablo.aa@gmail.com>
2025-01-19 16:55:03 +0200

    f

--- 6_recursion/3_use_recursion/2_write/flatten_nested_list.py
+++ 6_recursion/3_use_recursion/2_write/flatten_nested_list.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from trace_recursion import trace_recursion
 
 
+@trace_recursion
 def flatten_nested_list(nested_list: list) -> list:
     """
     flatten_nested_list flattens a nested list.
